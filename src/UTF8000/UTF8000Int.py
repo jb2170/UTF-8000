2454ddf88438c213fda3b2dc940067bfc4c63a7b
jb2170 <email@jb2170.com>
2024-11-26 18:19:25 +0000

    First main commit to UTF-8000

---
+++ src/UTF8000/UTF8000Int.py
@@ -0,0 +1,31 @@
+from .UTF8000Byte import UTF8000Byte
+
+class UTF8000Int:
+    def __init__(self, utf_8000_bytes: list[UTF8000Byte]) -> None:
+        # No validation is done; we're assuming these come from `UTF8000IncrementalDecoder`
+        self.utf_8000_bytes = utf_8000_bytes
+
+    def __str__(self) -> str:
+        return " ".join(str(b) for b in self.utf_8000_bytes)
+
+    def debug_str(self) -> str:
+        return " ".join(b.debug_str() for b in self.utf_8000_bytes)
+
+    def __int__(self) -> int:
+        ret = 0
+        content_bytes = (b for b in self.utf_8000_bytes if b.is_content_byte)
+        for content_byte in content_bytes:
+            ret <<= content_byte.n_content_bits
+            ret += content_byte.content
+        return ret
+
+    @property
+    def n_bytes(self) -> int:
+        return len(self.utf_8000_bytes)
+
+    @property
+    def n_bits_capacity(self) -> int:
+        if self.n_bytes == 1:
+            return 7
+        else:
+            return 1 + 5 * self.n_bytes
